aabd615da28538b8a00b17179ef4381702fce999
Ameer <ak2260650@gmail.com>
2022-05-18 19:40:19 +0530

    trending movies card finished

--- src/components/trendingCard/TrendingCard.js
+++ src/components/trendingCard/TrendingCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./TrendingCard.module.scss";
 import { IMG_URL } from "../../apis/movieDpApi";
+import movieIcon from "../../assets/icon-category-movie.svg";
+import seriesIcon from "../../assets/icon-category-tv.svg";
 
 const TrendingCard = ({ data }) => {
   console.log(data);
@@ -17,19 +19,19 @@ const TrendingCard = ({ data }) => {
           <div className={styles.release_date}>
             {data.release_date.slice(0, 4)}
           </div>
-          <span>.</span>
+
           <div className={styles.category}>
-            <svg
+            <img
               className={styles.category__icon}
-              width="20"
-              height="20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z" />
-            </svg>
+              src={`${data.media_type === "movie" ? movieIcon : seriesIcon}`}
+              alt=""
+            />
             <p className={styles.category__name}>{data.media_type}</p>
           </div>
-          <span>.</span>
+
+          <div className={styles.age_limit}>{`${
+            data.adult ? "18+" : "PG"
+          }`}</div>
         </div>
 
         <div className={styles.title}>{data.original_title}</div>
